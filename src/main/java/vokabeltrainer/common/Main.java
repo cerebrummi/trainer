a3adf882b1c79e2813371092c17415a49cb343c2
birkeheeren <internet@copepod.de>
2021-03-14 21:10:30 +0100

    main new Nutzungszeit 2022

--- src/main/java/vokabeltrainer/common/Main.java
+++ src/main/java/vokabeltrainer/common/Main.java
@@ -41,12 +41,12 @@ public final class Main
    {
 
       LocalDate thisPointInTime = LocalDate.now();
-      LocalDate endOfTest = LocalDate.of(2022, 1, 1);
+      LocalDate endOfTest = LocalDate.of(2023, 1, 1);
 
       if (endOfTest.isBefore(thisPointInTime))
       {
          JOptionPane.showMessageDialog(null,
-               "Die Nutzungszeit für diese Testversion Jahr 2021\nvon Cerebrummi© Ivrit 1.0 ist abgelaufen.\nInformationen unter kontakt@adaadama.com",
+               "Die Nutzungszeit für diese Testversion Jahr 2022\nvon Cerebrummi© Ivrit 1.0 ist abgelaufen.\nInformationen unter kontakt@adaadama.com",
                "Nachricht", JOptionPane.CLOSED_OPTION);
          System.exit(0);
       }
@@ -176,7 +176,7 @@ public final class Main
          window.setIconImage(ApplicationImages.getLogo());
          vokabeltrainer.common.Common.setjFrame(window);
          window.setTitle(
-               "Cerebrummi© Ivrit 1.0 Testversion Jahr 2021 | Nutzungszeit bis 31.12.2021");
+               "Cerebrummi© Ivrit 1.0 Testversion Jahr 2022 | Nutzungszeit bis 31.12.2022");
          window.setFont(germanFont.deriveFont(14F));
          ToolTipManager.sharedInstance().setDismissDelay(8000);
          ToolTipManager.sharedInstance().setInitialDelay(1000);
