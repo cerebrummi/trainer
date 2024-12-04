7659b567d3e81f4058f1f32620401460f074c88b
birkeheeren <kontakt@dornrose.art>
2024-12-04 08:22:07 +0100

    Start von version 6.0

--- src/main/java/vokabeltrainer/common/Settings.java
+++ src/main/java/vokabeltrainer/common/Settings.java
@@ -35,7 +35,7 @@ public class Settings
    private static float volume = -20;
    private static boolean letterImagesOn = true;
    private static Selection languageInput = readLanaguageInput();
-   private static String version = "5.0";
+   private static String version = "6.0";
 
    private static LinkedList<Database> oldChosenDatabases = new LinkedList<>();
    private static LinkedList<Database> chosenDatabases = new LinkedList<>();
